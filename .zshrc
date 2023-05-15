8933dd06a220b3e2dec8ad4f81fa6e98ec8b701c
三洋三洋 <56267907+SPLEEHYS@users.noreply.github.com>
2023-05-15 13:52:24 +0800

    2023-05-15 13:52

--- .zshrc
+++ .zshrc
@@ -4,7 +4,7 @@ fish
 PS1="> "
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
+!! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/Users/Leehy/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -17,4 +17,3 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-
--- config.fish
+++ config.fish
@@ -92,6 +92,13 @@ if test -f /Users/Leehy/anaconda3/bin/conda
 end
 # <<< conda initialize <<<
 
+# Using tmux with conda, there will be a problem:
+#+ When open a new terminal in tmux will auto activate base virenv 
+#+ and pacakage will be system instead of base virenv scope, 
+#+ so one of the solution is deactivate current virenv and reactivate base virenv
+conda deactivate
+conda activate base
+
 # To use conda in fish shell, run `conda init fish` in zsh/bash when conda 
 #+ have been installed.
 # To config conda, edit ".condatc" dotfile.
