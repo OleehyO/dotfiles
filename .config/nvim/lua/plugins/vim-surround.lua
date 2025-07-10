d28eab23f1fbb241a6c263051cb9c5eedc195769
OleehyO <leehy0357@gmail.com>
2025-07-10 12:54:24 +0800

    [nvim] Add vim-surround

---
+++ .config/nvim/lua/plugins/vim-surround.lua
@@ -0,0 +1,10 @@
+return {
+    "tpope/vim-surround",
+    keys = {
+      { "ys", mode = "n" },
+      { "ds", mode = "n" },
+      { "cs", mode = "n" },
+      { "S",  mode = "v" }, -- 启用视觉模式下的 S 快捷键
+    },
+    event = "VeryLazy",
+  }
